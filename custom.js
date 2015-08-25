6f1952fbb99472146c317644e86676b06123aad2
Kelsey McKenna <kelseyguitar55@googlemail.com>
2015-08-25 01:32:38 +0100

    A section for a food item is automatically added to the page using javascript.

--- custom.js
+++ custom.js
@@ -3,5 +3,53 @@
  */
 
 function initialise() {
-    
+    addItemSection();
 }
+
+function addItemSection() {
+    var newItemSection = createItemSection();
+    document.getElementById("main").appendChild(newItemSection);
+}
+
+function createItemSection() {
+    var itemSection = document.createElement("div");
+    itemSection.setAttribute("class", "itemSection");
+
+    var nameDiv = document.createElement("div");
+    var nameInput = document.createElement("input");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("class", "name");
+    nameInput.setAttribute("placeholder", "Food Item");
+    nameDiv.appendChild(nameInput);
+
+    var timeRemainingDiv = document.createElement("div");
+    var timeRemainingDisplay = document.createElement("div");
+    timeRemainingDisplay.setAttribute("class", "timeRemaining");
+    timeRemainingDisplay.innerHTML = "--:--";
+    var timeRemainingInput = document.createElement("input");
+    timeRemainingInput.setAttribute("type", "time");
+    timeRemainingInput.setAttribute("class", "timeEditor");
+    var btnSubmit = document.createElement("button");
+    btnSubmit.setAttribute("class", "btnSubmit");
+    btnSubmit.innerHTML = "Submit";
+    timeRemainingDiv.appendChild(timeRemainingDisplay);
+    timeRemainingDiv.appendChild(timeRemainingInput);
+    timeRemainingDiv.appendChild(btnSubmit);
+
+    var btnRemove = document.createElement("button");
+    btnRemove.setAttribute("class", "remove");
+
+    var progressBar = document.createElement("div");
+    progressBar.setAttribute("class", "progressBar");
+
+    itemSection.appendChild(nameDiv);
+    itemSection.appendChild(timeRemainingDiv);
+    itemSection.appendChild(btnRemove);
+    itemSection.appendChild(progressBar);
+
+    return itemSection;
+}
+
+
+
+
